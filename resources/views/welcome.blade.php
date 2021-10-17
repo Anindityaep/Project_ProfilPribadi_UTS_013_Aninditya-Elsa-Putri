27e656fad67c97fe638837e284240fce99d088a3
Aninditya Elsa Putri <aninditya.19013@mhs.unesa.ac.id>
2021-10-17 18:24:05 +0700

    Project_ProfilPribadi_UTS_013_Aninditya-Elsa-Putri

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Biodata Profil</title>
+    <link rel="icon" type="image/x-icon" href="{{url('tampilan/assets/img/favicon.ico')}}" />
+        <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{url('tampilan/css/styles.css')}}" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Pemrograman Basis Data</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{url('tampilan/assets/img/profile.jpeg')}}" alt="" /></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Tentang</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Pengalaman</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Pendidikan</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+        <!-- About-->
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Aninditya
+                    <span class="text-primary">Elsa Putri</span>
+                </h1>
+                <div class="subheading mb-5">
+                    Magersari Permai Blok AZ - 22, Sidoarjo (083849077877)
+                    <a href="mailto:name@email.com">aninditya.19013@mhs.unesa.ac.id</a>
+                </div>
+                <p class="lead mb-5">Halo, Perkenalkan nama saya Aninditya Elsa Putri. Saya berasal dari Sidoarjo lahir tanggal 7 bulan Oktober tahun 2000. Saya sekarang menempuh kuliah di Universitas Negeri Surabaya semester 5 Jurusan Teknik Informatika Prodi S1 Pendidikan Teknologi Informasi. Saya anak ke 3 dari 3 bersaudara. </p>
+                <div class="social-icons">
+                    <a class="social-icon" href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="social-icon" href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Experience-->
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Pengalaman</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Quality Control (QC)</h3>
+                        <div class="subheading mb-3">Akifa Official</div>
+                        <p>Pernah bekerja di suatu produk gamis Akifa Collection menjadi Quality Control di Sidoarjo. </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">2 Bulan</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">HTML, CSS, dan Java Script</h3>
+                        <div class="subheading mb-3"></div>
+                        <p>Pada semster 4 lalu sempat belajar HTML, CSS, dana Java Script di mata kuliah Pemrograman Web</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary"></span>5 bulan</div>
+                </div>
+              </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Education-->
+        <section class="resume-section" id="education">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Pendidikan</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Universitas Negeri Surabaya</h3>
+                        <div class="subheading mb-3">Pendidikan Teknologi Informasi</div>
+                        <!-- <div>Computer Science - Web Development Track</div>
+                        <p>GPA: 3.23</p> -->
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">August 2019 - Now</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SMAN 1 GEDANGAN</h3>
+                        <div class="subheading mb-3">MIPA</div>
+                        <!-- <p>GPA: 3.56</p> -->
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Juni 2017 - Juni 2019</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SMPN 5 SIDOARJO</h3>
+                        <div class="subheading mb-3"></div>
+                        <!-- <p>GPA: 3.56</p> -->
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Juni 2015 - Juni 2017</span></div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="{{url('tampilan/js/scripts.js')}}"></script>
+</body>
+
+</html>
